7813fc59c4470bd74acf1b5897d7a0ea77f3ab7e
Rendy Arya Kemal <rendyarya22@gmail.com>
2019-07-10 17:10:09 +0700

    Add ability to use custom checkpoint

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from notAiess import notAiess
 import click
+from datetime import datetime
 
 @click.command()
+@click.option("--datentime", "-d", help="Starting datetime point")
 @click.argument('apikey')
 @click.argument('hookurl')
-def main(apikey, hookurl):
-    nA = notAiess(apikey, hookurl)
+def main(apikey, hookurl, datentime):
+    dt = datetime.strptime(datentime, '%Y-%m-%dT%H:%M:%S+00:00')
+    nA = notAiess(apikey, hookurl, dt)
     nA.run()
 
 if __name__ == "__main__":
