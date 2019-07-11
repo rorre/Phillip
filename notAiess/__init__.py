58734634e0359ce2fb87ef2348e778d69bf551ec
Rendy Arya Kemal <rendyarya22@gmail.com>
2019-07-11 16:43:12 +0700

    Allow to import Handler

--- notAiess/__init__.py
+++ notAiess/__init__.py
@@ -1,3 +1,3 @@
 from .__version__ import __title__, __description__, __url__, __version__
 from .__version__ import __author_email__, __author__, __license__
-from .notaiess import notAiess
+from .notaiess import notAiess, Handler
