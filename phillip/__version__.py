edc2019ff6071dfec677bba482d6d5349940dae2
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-06-02 09:24:44 +0700

    Version bump

--- phillip/__version__.py
+++ phillip/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "Phillip"
 __description__ = "An event-driven for osu! feeds."
 __url__ = "https://github.com/rorre/Phillip"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Rendy Arya Kemal"
 __author_email__ = "rendyarya22@gmail.com"
 __license__ = "LGPLv3"
