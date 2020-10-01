8bfbb9d76b040de976d0bb067642c3c8ecf7e77b
Rendy Arya Kemal <rendyarya22@gmail.com>
2020-10-01 12:56:50 +0700

    flake8: Ignore all __init__.pys

--- phillip/__init__.py
+++ phillip/__init__.py
@@ -4,6 +4,6 @@ Phillip
 
 A basic wrapper for osu! beatmapset events
 """
-
+# flake8: noqa
 from .__version__ import (__author__, __author_email__, __description__,
                           __license__, __title__, __url__, __version__)
--- phillip/osu/new/__init__.py
+++ phillip/osu/new/__init__.py
@@ -1,2 +1,3 @@
+# flake8: noqa
 from .api import APIClient
 from .web import WebClient
